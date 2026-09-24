e94b7304b813ec422957af419761f7aa3324a357
Dharitri <dharitri@Dharitris-Laptop.local>
2026-09-24 21:48:11 +0530

    Connect frontend to deployed backend

--- frontend/src/pages/Prediction.jsx
+++ frontend/src/pages/Prediction.jsx
@@ -58,7 +58,7 @@ function Prediction() {
     setResult("")
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://insurance-fraud-detection-v7bw.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
